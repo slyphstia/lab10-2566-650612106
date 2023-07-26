e2770f968a99625f5a9d308a812eb56a11761675
hghgh <apiwit_boonyarit@cmu.ac.th>
2023-07-26 16:49:52 +0700

    completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,17 @@
 "use client";
 
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -19,14 +23,40 @@ export default function RandomUserPage() {
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
+    const cleanedUser = [];
+    for (let i = 0; i < users.length; i++) {
+      cleanedUser[i] = cleanUser(users[i]);
+    }
+    const newusered = [...cleanedUser];
+    setUsers(newusered);
   };
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoad(false);
+      return;
+    }
+    const strGenAmount = JSON.stringify(genAmount);
+    localStorage.setItem("genAmount", strGenAmount);
+  }, [genAmount]);
+
+  useEffect(() => {
+    const strGenAmount = localStorage.getItem("genAmount");
+    if (strGenAmount === null) {
+      setGenAmount(1);
+      return;
+    }
+    
+    const loadedGenAmount = JSON.parse(strGenAmount);
+    setGenAmount(loadedGenAmount);
+  }, []);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
       <p className="display-4 text-center fst-italic m-4">Users Generator</p>
       <div className="d-flex justify-content-center align-items-center fs-5 gap-2">
         Number of User(s)
-        <input
+        <input 
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
@@ -40,7 +70,17 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((x) => (
+          <UserCard
+            name={x.name}
+            imgUrl={x.imgUrl}
+            address={x.address}
+            email={x.email}
+            key={x.key}
+          />
+        ))}
     </div>
   );
 }
